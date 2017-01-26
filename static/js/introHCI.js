a635e5c54b6b9e72a818dfc004bace1fdea74c40
tamaresserman <tamar.esserman@gmail.com>
2017-01-26 09:43:08 -0800

    lab 3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -30,5 +30,5 @@ function projectClick(e) { 
        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
     } else {
         description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
-    }
+    } 
 }
